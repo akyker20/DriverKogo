d49c305d38afc4a355bd69cc7df399ba20078dcc
Austin Kyker <amk66@duke.edu>
2014-10-14 20:10:24 -0400

    refactored and documented Controller and GUIController

--- src/control/Controller.java
+++ src/control/Controller.java
@@ -26,9 +26,14 @@ import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+/**
+ * This class serves as a controller between Video (the backend) and the
+ * GUIController  (the head of the front-end). It promotes a seperation
+ * between front-end and back-end.
+ * @author Austin Kyker
+ *
+ */
 public class Controller extends Application {
-
-	public static final int NUM_DRIVERS = 8;
 
 	private GUIController myGUIController;
 	private VideoSelector myVideoSelector;
@@ -47,6 +52,17 @@ public class Controller extends Application {
 		myGUIController = new GUIController(myStage, this);
 	}
 
+	/**
+	 * This method is called after the driver drags and drops the XML File.
+	 * It initializes the XMLController and allows the GUIController to
+	 * configure the ride screen.
+	 * @param file
+	 * @throws FileNotFoundException
+	 * @throws ParserConfigurationException
+	 * @throws SAXException
+	 * @throws IOException
+	 * @throws TransformerException
+	 */
 	public void initializeDrivingEnvironment(File file) 
 			throws FileNotFoundException, ParserConfigurationException, 
 			SAXException, IOException, TransformerException {
@@ -56,12 +72,24 @@ public class Controller extends Application {
 		myGUIController.configureDrivingEnvironment();
 	}
 
+	/**
+	 * When a video is played, myNumPassengers is set so that when another
+	 * video is played this class has access to the number of passengers.
+	 * A video is selected and that video is passed to the GUIController
+	 * so that the stage will show the video-playing scene.
+	 * @param numPassengers
+	 */
 	public void playVideo(int numPassengers){
 		myNumPassengers = numPassengers;
 		Video videoToBePlayed = myVideoSelector.selectVideo();
 		myGUIController.playVideo(videoToBePlayed);
 	}
 
+	/**
+	 * If any videos exist that still have views, the playVideo function
+	 * is called. Otherwise, the GUIController is called to display the
+	 * not enough videos scene.
+	 */
 	public void playAnotherVideo() {
 		if(canPlayVideos())
 			playVideo(myNumPassengers);		
@@ -69,33 +97,57 @@ public class Controller extends Application {
 			myGUIController.notEnoughVideos();
 	}
 
+	/**
+	 * If there any videos that have not been played this ride, a random
+	 * video is selected from this set. Otherwise, if there are any videos
+	 * that have views remaining, a random video from this set is selected.
+	 * @return
+	 */
 	public Object[] playableVideos(){
 		Object[] playableUnplayedVideos = myVideoList.stream()
 				.filter(Video::canPlay).toArray();
-		//if there are videos that have not been played this ride
-		//and have available views.
 		if (playableUnplayedVideos.length > 0)
 			return playableUnplayedVideos;
-		//if there are videos that have available views.
 		else{
 			return myVideoList.stream()
 					.filter(Video::hasPlaysRemaining).toArray();
 		}
 	}
 
+	/**
+	 * If there are videos that have not been played this ride, or
+	 * if there are videos that have remaining views.
+	 * @return
+	 */
 	public boolean canPlayVideos() {
 		return playableVideos().length > 0;
 	}
 
+	/**
+	 * Called when a video is completed. The video's play attribute is incremented
+	 * according to the number of passengers.
+	 * The master XML File is edited to reflect these changes.
+	 * @param videoCompleted
+	 * @throws TransformerException
+	 */
 	public void completedVideo(Video videoCompleted) throws TransformerException {
 		videoCompleted.addViews(myNumPassengers);
 		myXMLController.updateXML(videoCompleted, myNumPassengers);
 	}
 
+	/**
+	 * Adds the finished driving scene to the stage. This method is called when
+	 * the driver clicks File -> Finished Driving.
+	 */
 	public void finishDriving() {
 		myGUIController.showFinishedDrivingScreen();
 	}
 
+	/**
+	 * Removes the video playing scene from the stage and replaces it with
+	 * the start ride screen so the driver can start a new ride. This method is
+	 * called when the driver clicks File -> Ride Completed.
+	 */
 	public void endRide() {
 		myGUIController.showStartRideScreen();
 	}
--- src/gui/GUIController.java
+++ src/gui/GUIController.java
@@ -8,11 +8,17 @@ import gui.scenes.NotEnoughVideosScene;
 import gui.scenes.RideStarterScene;
 import gui.scenes.VideoScene;
 import javafx.scene.Group;
-import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import control.Controller;
 
+/**
+ * This is the controller of the front-end. It has reference to the controller
+ * and is responsible for initializing and modifying the GUI by switching 
+ * between different scenes when appropriate.
+ * @author Austin Kyker
+ *
+ */
 public class GUIController {
 
 	public static final int SCREEN_WIDTH = 600;
@@ -22,9 +28,7 @@ public class GUIController {
 	private static final String START_RIDE_TITLE = "Start Ride";
 	private static final String FINISHED_RIDING_TITLE = "Finished Riding";
 	private static final String NO_MORE_VIDEOS_TITLE = "No More Videos";
-	
-	public static final String TITLE = "Kogo Driver";
-	public static final String STYLESHEET_PACKAGE = "Stylesheets/";
+	private static final String DRAG_AND_DROP_TITLE = "Drag Kogo File";
 
 	private Controller myController;
 	private RideStarterScene myRideStarterScene;
@@ -45,13 +49,22 @@ public class GUIController {
 		configureStageAndDisplayDragFileScene();
 	}
 
+	/**
+	 * On load, the drag and drop screen is displayed to the user. The program needs
+	 * the driver XML File before it can start showing advertisement clips.
+	 */
 	private void configureStageAndDisplayDragFileScene(){
-		myStage.setTitle("Drag Kogo File");
+		myStage.setTitle(DRAG_AND_DROP_TITLE);
 		myStage.setResizable(false);
 		myStage.setScene(myDragFileScreen);
 		myStage.show();
 	}
 
+	/**
+	 * Adds the video playing scene to the stage. Sets the stage to be full screen
+	 * and presents the driver with a hint on how to pause and complete rides.
+	 * @param video
+	 */
 	public void playVideo(Video video){
 		myVideoScene.setUpVideo(video);
 		myStage.setScene(myVideoScene);
@@ -60,30 +73,57 @@ public class GUIController {
 		myStage.setFullScreen(true);
 	}
 	
+	/**
+	 * Makes the stage full screen. This method is called when the driver hits escape
+	 * to exit full screen and then hits SPACE to play the video again.
+	 */
 	public void makeFullScreen() {
 		myStage.setFullScreenExitHint("Press spacebar to pause and complete ride");
 		myStage.setFullScreen(true);
 	}
 	
+	/**
+	 * Called when driver hits SPACE to play the video. If the stage is not
+	 * in full screen, then it is set to be full screen with makeFullScreen.
+	 * @return true if the stage is in full screen.
+	 */
 	public boolean isFullScreen(){
 		return myStage.isFullScreen();
 	}
 
+	/**
+	 * Displays the start ride screen to the driver which asks for the number of
+	 * students in the ride.
+	 */
 	public void showStartRideScreen(){
 		myStage.setScene(myRideStarterScene);
 		myStage.setTitle(START_RIDE_TITLE);
 	}
 
+	/**
+	 * If no videos exist with unused views, the driver is shown the
+	 * "Not Enough Videos Screen" and instructed that they will not be 
+	 * paid for continued driving. Hopefully, drivers won't see this screen!
+	 */
 	public void notEnoughVideos() {
 		myStage.setScene(myNotEnoughVideosScene);
 		myStage.setTitle(NO_MORE_VIDEOS_TITLE);
 	}
 
+	/**
+	 * The Finished Driving Scene is displayed to the user after they select
+	 * "Finish Driving" from the file menu. They are instructed to send their
+	 * XML file to the owners.
+	 */
 	public void showFinishedDrivingScreen() {
 		myStage.setScene(myFinishedDrivingScene);
 		myStage.setTitle(FINISHED_RIDING_TITLE);
 	}
 
+	/**
+	 * If there are videos to play, the start ride screen is displayed.
+	 * Otherwise, the "Not Enough Videos" screen is shown.
+	 */
 	public void configureDrivingEnvironment() {
 		if(myController.canPlayVideos())
 			showStartRideScreen();
